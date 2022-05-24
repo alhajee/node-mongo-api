1f46ed17294dd019204270d8b92de07317514122
Muhammad Alhajee <alhajee2009@gmail.com>
2022-05-24 17:54:44 +0100

    create CRUD operations

---
+++ users/daos/users.dao.ts
@@ -0,0 +1,73 @@
+import { CreateUserDto } from "../dto/create.user.dto";
+import { PatchUserDto } from "../dto/patch.user.dto";
+import { PutUserDto } from "../dto/put.user.dto";
+
+import shortid from "shortid";
+import debug from "debug";
+
+const log: debug.IDebugger = debug("app:in-memory-dao");
+
+class UsersDao {
+  users: Array<CreateUserDto> = [];
+
+  constructor() {
+    log("Created new instance of UsersDao");
+  }
+
+  async addUser(user: CreateUserDto) {
+    user.id = shortid.generate();
+    this.users.push(user);
+    return user;
+  }
+
+  async getUsers() {
+    return this.users;
+  }
+
+  async getUserById(userId: string) {
+    return this.users.find((user: { id: string }) => user.id === userId);
+  }
+
+  async putUserById(userId: string, user: PutUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+    this.users.splice(objIndex, 1, user);
+    return `${user.id} updated via put`;
+  }
+
+  async patchUserById(userId: string, user: PatchUserDto) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+    const currentUser = this.users[objIndex];
+    const allowedPatchFields = [
+      "password",
+      "firstName",
+      "lastName",
+      "permissionLevel",
+    ];
+    for (let field of allowedPatchFields) {
+      if (field in user) {
+        // @ts-ignore
+        currentUser[field] = user[field];
+      }
+    }
+    this.users.splice(objIndex, 1, currentUser);
+    return `${user.id} updated via patch`;
+  }
+
+  async removeUserById(userId: string) {
+    const objIndex = this.users.findIndex(
+      (obj: { id: string }) => obj.id === userId
+    );
+    this.users.splice(objIndex, 1);
+    return `${userId} deleted`;
+  }
+
+  async getUserByEmail(email: string) {
+    return this.users.find((user: { email: string }) => user.email === email);
+  }
+}
+
+export default new UsersDao();
